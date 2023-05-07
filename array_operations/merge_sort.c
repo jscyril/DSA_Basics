4d685165925c09b9e8aa6c933a34514ebaa54f8f
jscyril <112850923+jscyril@users.noreply.github.com>
2023-05-08 01:31:31 +0530

    Update merge_sort.c

--- array_operations/merge_sort.c
+++ array_operations/merge_sort.c
@@ -9,8 +9,6 @@ void merge(int arr[], int l, int m, int r){
     for(j = 0; j < n2; j++)
         arr_r[j] = arr[m + 1 + j];
     i = 0,j = 0,k = l; 
-    printf("\n\t(VALUE OF N1:%d)", n1);
-    printf("\n\t(VALUE OF N2:%d)", n2);
     while(i < n1 && j < n2) {
         if(arr_l[i] <= arr_r[j]){
             arr[k]=arr_l[i];
@@ -22,8 +20,6 @@ void merge(int arr[], int l, int m, int r){
         }
         k++;
     }
-    printf("\n\t(VALUE OF I:%d)", i);
-    printf("\n\t(VALUE OF J:%d)", j);
     while(i < n1){
         arr[k]=arr_l[i];
         i++;
@@ -38,12 +34,7 @@ void merge(int arr[], int l, int m, int r){
 void mergeSort(int arr[], int l, int r){
     if(l<r){   
         int m =l+(r-l)/2;
-        printf("\n\n\n\n(VALUE_OF_MID:%d)",m);
-        printf("\n\n(FIRST_RECURSION)Value of lower and mid is:%d\t%d\t",l,m);
-        mergeSort(arr,l,m);
-        printf("\n\n(SECOND_RECURSION)Value of mid+1 and right is:%d\t%d\t",m+1,r);
         mergeSort(arr,m+1,r);
-        printf("\n\n(THRID_RECURSION)Value of lower and mid and right is:%d\t%d\t%d\t",l,m,r);
         merge(arr,l,m,r);
     }
 }
